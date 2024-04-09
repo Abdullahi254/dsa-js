005fe6bbabde3dad193ad1bf08633247c2d64cf5
Abdullahi254 <abdush504@gmail.com>
2024-04-09 14:25:01 +0300

    extracting max node in max binary heap

--- heap.js
+++ heap.js
@@ -17,9 +17,9 @@ class MaxBinaryHeap {
         } else {
             // pushing the element to the list of values
             this.values.push(val)
-            let n = this.values.length -1
+            let n = this.values.length - 1
             let temp
-            // bubbling the val to be inserted in the correct index
+            // bubbling up the val to be inserted in the correct index
             // while the child is greater than the parent the loop will continue to iterate
             while (this.values[n] > this.values[Math.floor((n - 1) / 2)]) {
                 //swaping if the child is greater than the parent
@@ -30,11 +30,52 @@ class MaxBinaryHeap {
             }
         }
     }
+    // removing the max element which is the root node and then sorting the heap for every node to be place right
+    extractMax() {
+        if (this.values.length < 1) {
+            return this.values[0]
+        } else {
+            //getting the last element
+            let lastElement = this.values[this.values.length - 1]
+
+            // placing the last element to be the root node
+            this.values[0] = lastElement
+            // poping the last element to remove it at the end
+            this.values.pop()
+            // getting the first element to use it when bubbling down to place it in the right place
+            let node = this.values[0]
+
+            let n = 0
+            let childOne = this.values[1]
+            let childTwo = this.values[2]
+            // while node is smaller than either of it's children, continue to bubble it down
+            while (node < childOne || node < childTwo) {
+                if (childOne >= childTwo) {
+                    this.values[n] = childOne
+                    this.values[(2 * n) + 1] = node
+                    node = this.values[(2 * n) + 1]
+                    n = (2 * n) + 1
+                }
+                else if (childTwo > childOne) {
+                    this.values[n] = childTwo
+                    this.values[(2 * n) + 2] = node
+                    node = this.values[(2 * n) + 2]
+                    n = (2 * n) + 2
+                }
+                childOne = this.values[(2 * n) + 1]
+                childTwo = this.values[(2 * n) + 2]
+            }
+        }
+    }
     print() {
         console.log(this.values)
     }
 }
 
 const myHeap = new MaxBinaryHeap()
-myHeap.insert(41)
+// myHeap.insert(41)
+console.log("BEFORE EXTRACTION ***********************")
+myHeap.print()
+myHeap.extractMax()
+console.log("AFTER EXTRACTION ***********************")
 myHeap.print()
